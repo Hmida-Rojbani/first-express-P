b37cc6d213d9a86d9f18e3daf8c320215eec0cd8
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-03 20:56:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('',function (req,res) {
+const students = [
+    {id : 1, name : 'Student1'},
+    {id : 2, name : 'Student2'},
+    {id : 3, name : 'Student3'}
+]
+
+app.get(['','/index'],function (req,res) {
     res.send('<H1> Bonsoir GLSI-P </H1>')
 });
 
-app.listen(3000, ()=> console.log('Server on 3000...'));
+app.get('/api/students',function (req,res) {
+    res.send(students)
+});
+
+app.get('/api/students/:id',function (req,res) {
+    let student = students.find(s => s.id === parseInt(req.params.id));
+    if(!student)
+        return res.status(404).send('Student with this id is not found');
+    res.send(student)
+});
+app.use(express.json());
+app.post('/api/students',(req,res) => {
+    let student = {
+        id : students.length + 1,
+        name: req.body.name
+    }
+    students.push(student);
+    res.status(201).send(student);
+});
+
+// put an delete
+app.listen(port, ()=> console.log(`Server on ${port}...`));
